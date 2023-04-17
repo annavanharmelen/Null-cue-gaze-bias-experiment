2da7c9b492f346dc8d461e12c070dc90fba6b542
Anna <58181940+annavanharmelen@users.noreply.github.com>
2023-04-17 11:46:16 +0200

    change testing data repo

--- set_up.py
+++ set_up.py
@@ -21,7 +21,7 @@ def get_monitor_and_dir(testing: bool):
             "distance": 50,  # in cm
         }
 
-        directory = r"C:\Users\annav\Documents\Jottacloud\Neuroscience\Year 2\Master's thesis\Code\Null-cue gaze bias\data"
+        directory = r"C:\Users\annav\Documents\Jottacloud\Neuroscience\Year 2\Master's thesis\Data\test"
 
     else:
         # lab
